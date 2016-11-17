e6a49c7ae00fd01325a6d5a892b651d5bb00540b
Amos <amoswhitewolf@gmail.com>
2016-11-17 11:10:12 +1100

    Update header styling

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ var gulp = require('gulp'),
   autoprefixer = require('gulp-autoprefixer'),
   sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
-  minifycss = require('gulp-minify-css'),
   rename = require('gulp-rename'),
   livereload = require('gulp-livereload'),
   lr = require('tiny-lr'),
@@ -22,7 +21,7 @@ var gulp = require('gulp'),
 // BROWSER SYNC
 livereload({ start: true });
 browserSync.init({
-  proxy: "future.dev"
+  proxy: "resolution.dev"
 });
 
 gulp.task('styles', function(){
@@ -44,8 +43,6 @@ gulp.task('styles', function(){
       })
     )
 
-    //.pipe(rename({ suffix: '.min' }))
-    //.pipe(minifycss())
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write())
     .pipe( gulp.dest('static/css') )
